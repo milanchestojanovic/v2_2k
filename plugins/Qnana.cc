0f1ac10b09917841d893fbef240868a8a05b1d3f
mstojano <milan.stojanovic@cern.ch>
2023-01-09 17:25:36 +0100

    updating with the latest analyzer

--- plugins/Qnana.cc
+++ plugins/Qnana.cc
@@ -35,14 +35,14 @@
 
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
 #include "FWCore/Utilities/interface/InputTag.h"
-//#include "DataFormats/TrackReco/interface/Track.h"
-//#include "DataFormats/TrackReco/interface/TrackFwd.h"
+#include "DataFormats/TrackReco/interface/Track.h"
+#include "DataFormats/TrackReco/interface/TrackFwd.h"
 #include "DataFormats/HeavyIonEvent/interface/CentralityBins.h"
 #include "DataFormats/HeavyIonEvent/interface/Centrality.h"
 #include <DataFormats/VertexReco/interface/Vertex.h>
 #include <DataFormats/VertexReco/interface/VertexFwd.h>
 
-//#include "2018PbPb_TrackingEfficiencies_Jan272020_Final/trackingEfficiency2018PbPb.h"
+#include "2018PbPb_TrackingEfficiencies_Jan272020_Final/trackingEfficiency2018PbPb.h"
 
 #include <TH1.h>
 #include <TH2.h>
@@ -86,7 +86,7 @@ class Qnana : public edm::one::EDAnalyzer<edm::one::SharedResources>  {
 
 
 //      std::vector<std::vector<TVector3> > event;//(2);
-/*
+
 Double_t phi_ref[maxmult];
 Double_t eff_ref[maxmult];
 Double_t weight[maxmult];
@@ -156,20 +156,20 @@ std::vector<Double_t> S101;
 
 
   Double_t multEv[16];	
-*/
+
 
 Int_t Low, High;
-int   centDefault, centNominal, centUp, centDown;
 
       TH1D *h_hiHF;
-      TH1D *h_hiHFdown;
-      TH1D *h_hiHFup;
 
-      TTree* VertexCompositeNtuple;
-/*
+      //TTree* VertexCompositeNtuple;
       TH3D *h_eff;
 
-      TH1D *h_multEv;
+  TH1D *h_multEv;
+  TH1D *h_pT;
+  TH1D *h_eta;
+  TH1D *h_Mult;
+  TH1D *h_M0; TH1D *h_M1; TH1D *h_M2; TH1D *h_M3; TH1D *h_M4; TH1D *h_M5; TH1D *h_M6; TH1D *h_M7; TH1D *h_M8; TH1D *h_M9; TH1D *h_M10; TH1D *h_M11; TH1D *h_M12; TH1D *h_M13; TH1D *h_M14; TH1D *h_M15;
 
       TH1D *h_W2; TH1D *h_W22; TH1D *h_W24; TH1D *h_W26; TH1D *h_W28; TH1D *h_W210; TH1D *h_W4; TH1D *h_W44; TH1D *h_W46; TH1D *h_W48; TH1D *h_W410; TH1D *h_W6; TH1D *h_W66; TH1D *h_W68; TH1D *h_W610; TH1D *h_W8; TH1D *h_W88; TH1D *h_W810; TH1D *h_W10; TH1D *h_W1010;
 
@@ -219,7 +219,7 @@ TH2D *h_CumSixt; TH2D *h_Six_vt; TH2D *h_ErrSix_vt;
 TH2D *h_CumEightt; TH2D *h_Eight_vt; TH2D *h_ErrEight_vt;
 TH2D *h_CumTent; TH2D *h_Ten_vt; TH2D *h_ErrTen_vt;
 //////
-*/
+
       bool useCentrality_;
 
       edm::EDGetTokenT<int> centralitySrc_;
@@ -249,7 +249,7 @@ TH2D *h_CumTent; TH2D *h_Ten_vt; TH2D *h_ErrTen_vt;
 
 
        Int_t Ntrk=0;
-       //TrkEff2018PbPb trkEff =  TrkEff2018PbPb("general", false, "");
+       TrkEff2018PbPb trkEff =  TrkEff2018PbPb("general", false, "");
 
 };
 
@@ -289,7 +289,7 @@ Qnana::Qnana(const edm::ParameterSet& iConfig)
 
 {
    //now do what ever initialization is needed
-/*
+
  Q.resize(5), Q2.resize(5), Q3.resize(5), Q4.resize(5), Q5.resize(5);
  W2Two.resize(5), W4Four.resize(5), W6Six.resize(5), W8Eight.resize(5), W10Ten.resize(5);
  SumW2Two.resize(5), SumW22Two.resize(5), SumW24Two.resize(5), SumW26Two.resize(5), SumW28Two.resize(5), SumW210Two.resize(5), SumW2TwoW2Two.resize(5);
@@ -422,7 +422,7 @@ Qnana::Qnana(const edm::ParameterSet& iConfig)
 //////
    }
  }
-*/
+
    edm::Service<TFileService> fs;
    initHistos(fs);
 
@@ -453,7 +453,7 @@ Qnana::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup)
 
 //   TrkEff2018PbPb trkEff =  TrkEff2018PbPb("general", false, "");
 
-/*
+
   Double_t pT;
   Double_t eta;
   Double_t pT_ref[maxmult]; 
@@ -461,11 +461,9 @@ Qnana::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup)
   Double_t M=0;
   Double_t Ntr=0;
   //Int_t Mi=0;
-*/
   Double_t HFsumET;
 
-   int occ=0;
-   int cbin=0;
+   int occ=0, cbin=0;
    if( useCentrality_ )
    {
      edm::Handle<reco::Centrality> cent;
@@ -478,12 +476,9 @@ Qnana::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup)
      //if ( occ < centMin_*2 || occ >= centMax_*2 ) return;
      HFsumET = cent->EtHFtowerSum();
      if (HFsumET > 5199.95) return; 
-     centDefault = occ; 
-     centNominal = 200 - h_hiHF->FindBin(HFsumET);
-     centDown    = 200 - h_hiHFdown->FindBin(HFsumET);
-     centUp      = 200 - h_hiHFup->FindBin(HFsumET);
+     occ = 200 - h_hiHF->FindBin(HFsumET);
    //  cout<<occ <<" " <<cbin<<endl;
-     //if ( occ >= 180 ) return;
+     if ( occ >= 160 ) return;
    }
    //if (occ <60 || occ>=70) return;
 
@@ -499,9 +494,6 @@ Qnana::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup)
      if( fabs(vsorted[0].z()) < vertexZMin_ ) return;
    }
    //std::cout<<"z position = "<<vsorted[0].z()<<endl;
-   VertexCompositeNtuple->Fill();
-        
-/*
    edm::Handle<edm::View<pat::PackedCandidate>> pfCandidates;
    iEvent.getByToken(srcPFcand_,pfCandidates);
 
@@ -520,6 +512,8 @@ Qnana::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup)
 
 	pT = itTrack.pt();
 	eta = itTrack.eta();
+	h_pT->Fill(pT);
+ 	h_eta->Fill(eta);
         double chi2n=(*chi2Map)[pfCandidates->ptrAt(i)];
 	if (pT>3.0) continue;
         if (pT<0.5) continue;
@@ -527,7 +521,11 @@ Qnana::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup)
         if ( itTrack.charge() == 0  ) continue;
         if ( ! passesTrackCuts(itTrack, vsorted, chi2n) ) continue;
         weight[int(Ntr)] = trkEff.getCorrection(itTrack.pt(), itTrack.eta(), occ);
-	eff_ref[int(Ntr)] = 1.0/trkEff.getCorrection(itTrack.pt(), itTrack.eta(), occ);
+        // eff. Nominal
+        eff_ref[int(Ntr)] = 1.0/(trkEff.getCorrection(itTrack.pt(), itTrack.eta(), occ));
+        // eff. Up 5%
+	//eff_ref[int(Ntr)] = 1.0/(trkEff.getCorrection(itTrack.pt(), itTrack.eta(), occ) + 0.05);
+
 	//eff_ref[int(Ntr)] = 0.6;
 	phi_ref[int(Ntr)] = itTrack.phi();
 
@@ -540,10 +538,28 @@ Qnana::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup)
     }
 
         Ntrk=Ntr;
+	h_Mult->Fill(Ntrk);
 	//VertexCompositeNtuple->Fill();
 	if (Ntr <11) return;
 	int l =0;
 	l = occ/2/5;
+
+        if (l==0) h_M0->Fill(Ntrk);
+        if (l==1) h_M1->Fill(Ntrk);
+        if (l==2) h_M2->Fill(Ntrk);
+        if (l==3) h_M3->Fill(Ntrk);
+        if (l==4) h_M4->Fill(Ntrk);
+        if (l==5) h_M5->Fill(Ntrk);
+        if (l==6) h_M6->Fill(Ntrk);
+        if (l==7) h_M7->Fill(Ntrk);
+        if (l==8) h_M8->Fill(Ntrk);
+        if (l==9) h_M9->Fill(Ntrk);
+        if (l==10) h_M10->Fill(Ntrk);
+        if (l==11) h_M11->Fill(Ntrk);
+        if (l==12) h_M12->Fill(Ntrk);
+        if (l==13) h_M13->Fill(Ntrk);
+        if (l==14) h_M14->Fill(Ntrk);
+        if (l==15) h_M15->Fill(Ntrk);
 
        multEv[l]++;
      //std::cout <<W22[l]<<std::endl;
@@ -1228,7 +1244,7 @@ Qnana::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup)
 
 
 
-*/
+
 
 #ifdef THIS_IS_AN_EVENT_EXAMPLE
    Handle<ExampleData> pIn;
@@ -1252,7 +1268,7 @@ Qnana::beginJob()
 void
 Qnana::endJob()
 {
-/*
+
    for (int l=0; l<16; l++) {
         h_multEv->SetBinContent(l+1,multEv[l]);
 
@@ -1503,7 +1519,7 @@ Qnana::endJob()
 
    }
  }
-*/
+
 }
 
 // ------------ method fills 'descriptions' with the allowed parameters for the module  ------------
@@ -1526,27 +1542,46 @@ void
 Qnana::initHistos(const edm::Service<TFileService> & fs)
 {
 
-  VertexCompositeNtuple = fs->make< TTree>("VertexCompositeNtuple","VertexCompositeNtuple");
+  //VertexCompositeNtuple = fs->make< TTree>("VertexCompositeNtuple","VertexCompositeNtuple");
 
-   VertexCompositeNtuple->Branch("centDefault",&centDefault,"centDefault/I");
-   VertexCompositeNtuple->Branch("centNominal",&centNominal,"centNominal/I");
-   VertexCompositeNtuple->Branch("centDown",&centDown,"centDown/I");
-   VertexCompositeNtuple->Branch("centUp",&centUp,"centUp/I");
+   //VertexCompositeNtuple->Branch("Ntrk",&Ntrk,"Ntrk/I");
+   //VertexCompositeNtuple->Branch("phi",phi_ref,"phi_ref[Ntrk]/D");
+   //VertexCompositeNtuple->Branch("eff",eff_ref,"eff_ref[Ntrk]/D");
 
   double hiHFbinedges[201] = {0, 10.5072, 11.2099, 11.8364, 12.478, 13.1194, 13.7623, 14.4081, 15.0709, 15.7532, 16.4673, 17.1881, 17.923, 18.673, 19.4865, 20.3033, 21.1536, 22.0086, 22.9046, 23.8196, 24.7924, 25.8082, 26.8714, 27.9481, 29.0828, 30.2757, 31.5043, 32.8044, 34.1572, 35.6142, 37.1211, 38.6798, 40.3116, 42.0398, 43.8572, 45.6977, 47.6312, 49.6899, 51.815, 54.028, 56.3037, 58.7091, 61.2024, 63.8353, 66.5926, 69.3617, 72.2068, 75.2459, 78.3873, 81.5916, 84.9419, 88.498, 92.1789, 95.9582, 99.8431, 103.739, 107.78, 111.97, 116.312, 120.806, 125.46, 130.269, 135.247, 140.389, 145.713, 151.212, 156.871, 162.729, 168.762, 174.998, 181.424, 188.063, 194.907, 201.942, 209.19, 216.683, 224.37, 232.291, 240.43, 248.807, 257.416, 266.256, 275.348, 284.668, 294.216, 304.053, 314.142, 324.488, 335.101, 345.974, 357.116, 368.547, 380.283, 392.29, 404.564, 417.122, 429.968, 443.116, 456.577, 470.357, 484.422, 498.78, 513.473, 528.479, 543.813, 559.445, 575.411, 591.724, 608.352, 625.344, 642.686, 660.361, 678.371, 696.749, 715.485, 734.608, 754.068, 773.846, 794.046, 814.649, 835.608, 856.972, 878.719, 900.887, 923.409, 946.374, 969.674, 993.435, 1017.62, 1042.21, 1067.28, 1092.72, 1118.64, 1144.96, 1171.71, 1198.98, 1226.67, 1254.82, 1283.46, 1312.65, 1342.21, 1372.27, 1402.85, 1433.93, 1465.49, 1497.62, 1530.29, 1563.49, 1597.22, 1631.49, 1666.37, 1701.8, 1737.75, 1774.35, 1811.51, 1849.29, 1887.75, 1926.79, 1966.6, 2006.97, 2047.99, 2089.71, 2132.1, 2175.23, 2219.17, 2263.72, 2309.2, 2355.43, 2402.47, 2450.33, 2499.05, 2548.66, 2599.16, 2650.59, 2703.03, 2756.32, 2810.75, 2866.27, 2922.91, 2980.54, 3039.47, 3099.53, 3160.98, 3223.66, 3287.71, 3353.18, 3420.34, 3489.13, 3559.72, 3632.06, 3706.18, 3782.42, 3860.78, 3941.42, 4024.52, 4110.27, 4199.4, 4292.8, 4394.49, 4519.52, 5199.95};
   double hiHFbinedgesSysDown[201] = {0, 10.5071, 11.2094, 11.8357, 12.4763, 13.117, 13.7597, 14.4049, 15.0671, 15.7491, 16.4622, 17.1812, 17.9144, 18.6674, 19.4797, 20.2963, 21.1435, 21.9974, 22.8928, 23.8068, 24.7805, 25.7931, 26.8556, 27.9308, 29.0638, 30.2582, 31.4795, 32.7816, 34.1349, 35.5834, 37.0941, 38.6474, 40.2782, 42.0035, 43.8112, 45.6576, 47.5758, 49.6381, 51.6667, 53.7353, 55.8903, 58.1259, 60.4528, 62.8712, 65.3859, 67.9968, 70.7065, 73.5231, 76.4519, 79.4922, 82.6461, 85.9264, 89.3269, 92.8562, 96.5212, 100.322, 104.262, 108.344, 112.585, 116.971, 121.521, 126.225, 131.09, 136.127, 141.328, 146.721, 152.284, 158.014, 163.935, 170.054, 176.372, 182.878, 189.602, 196.532, 203.653, 211.017, 218.599, 226.387, 234.418, 242.667, 251.16, 259.886, 268.852, 278.071, 287.498, 297.2, 307.184, 317.409, 327.894, 338.66, 349.686, 360.996, 372.607, 384.508, 396.669, 409.133, 421.86, 434.906, 448.258, 461.916, 475.906, 490.16, 504.74, 519.663, 534.911, 550.453, 566.322, 582.525, 599.08, 615.968, 633.211, 650.805, 668.76, 687.048, 705.707, 724.774, 744.163, 763.9, 783.999, 804.528, 825.432, 846.746, 868.429, 890.523, 913.007, 935.952, 959.211, 982.919, 1007.08, 1031.63, 1056.62, 1082.08, 1107.96, 1134.24, 1160.99, 1188.22, 1215.91, 1244.06, 1272.69, 1301.85, 1331.45, 1361.51, 1392.07, 1423.18, 1454.77, 1486.93, 1519.57, 1552.81, 1586.55, 1620.87, 1655.79, 1691.26, 1727.27, 1763.93, 1801.12, 1838.97, 1877.47, 1916.61, 1956.45, 1996.89, 2038.04, 2079.84, 2122.35, 2165.52, 2209.53, 2254.24, 2299.83, 2346.19, 2393.31, 2441.28, 2490.16, 2539.86, 2590.57, 2642.16, 2694.74, 2748.23, 2802.81, 2858.47, 2915.33, 2973.2, 3032.28, 3092.56, 3154.24, 3217.19, 3281.45, 3347.18, 3414.6, 3483.65, 3554.56, 3627.2, 3701.66, 3778.25, 3856.97, 3937.98, 4021.48, 4107.62, 4197.21, 4291.05, 4393.19, 4518.6, 5199.95}; 
   double hiHFbinedgesSysUp[201] = {0, 10.5075, 11.2107, 11.838, 12.4797, 13.1213, 13.7641, 14.4124, 15.0745, 15.7577, 16.473, 17.1939, 17.9297, 18.6812, 19.4958, 20.3143, 21.1648, 22.0218, 22.9159, 23.8328, 24.8059, 25.8204, 26.89, 27.9702, 29.1042, 30.3022, 31.528, 32.8347, 34.1896, 35.6439, 37.1542, 38.7172, 40.3518, 42.091, 43.9053, 45.7415, 47.6853, 49.7457, 51.8755, 54.0983, 56.3594, 58.7848, 61.2861, 63.9228, 66.6825, 69.4421, 72.297, 75.3547, 78.4967, 81.6977, 85.0755, 88.6211, 92.3058, 96.1071, 99.9975, 104.065, 108.272, 112.512, 116.906, 121.601, 126.465, 131.482, 136.866, 142.229, 147.786, 153.546, 159.571, 165.586, 171.902, 178.419, 185.063, 191.856, 199.055, 206.261, 213.999, 221.719, 229.671, 237.84, 246.088, 254.828, 263.883, 272.907, 282.236, 291.925, 301.519, 311.477, 321.691, 332.153, 342.892, 353.878, 365.161, 376.742, 388.577, 400.684, 413.075, 425.746, 438.711, 451.989, 465.556, 479.45, 493.608, 508.077, 522.891, 538.003, 553.415, 569.151, 585.216, 601.601, 618.354, 635.422, 652.84, 670.599, 688.699, 707.161, 726.014, 745.185, 764.687, 784.557, 804.838, 825.489, 846.537, 867.951, 889.752, 911.955, 934.588, 957.52, 980.912, 1004.73, 1028.94, 1053.57, 1078.67, 1104.17, 1130.07, 1156.39, 1183.2, 1210.47, 1238.17, 1266.38, 1295.02, 1324.16, 1353.71, 1383.77, 1414.35, 1445.41, 1477, 1509.09, 1541.74, 1574.88, 1608.59, 1642.83, 1677.66, 1713.07, 1748.98, 1785.47, 1822.63, 1860.33, 1898.72, 1937.73, 1977.42, 2017.71, 2058.62, 2100.25, 2142.57, 2185.56, 2229.38, 2273.91, 2319.2, 2365.33, 2412.22, 2459.94, 2508.52, 2557.98, 2608.35, 2659.61, 2711.86, 2765, 2819.23, 2874.58, 2930.97, 2988.46, 3047.12, 3106.95, 3168.15, 3230.6, 3294.37, 3359.58, 3426.47, 3494.95, 3565.21, 3637.21, 3711.03, 3786.91, 3864.85, 3945.11, 4027.8, 4113.06, 4201.73, 4294.72, 4395.9, 4520.5, 5199.95};
 
   h_hiHF = fs->make<TH1D>("hiHF edges"," ",200,hiHFbinedges);
-  h_hiHFdown = fs->make<TH1D>("hiHF edges SysDown"," ",200,hiHFbinedgesSysDown);
-  h_hiHFup = fs->make<TH1D>("hiHF edges SysUp"," ",200,hiHFbinedgesSysUp);
+  //h_hiHF = fs->make<TH1D>("hiHF edges"," ",200,hiHFbinedgesSysDown);
+  //h_hiHF = fs->make<TH1D>("hiHF edges"," ",200,hiHFbinedgesSysUp);
 
 
-/*
+
   h_eff  = fs->make<TH3D>("Efficiency" , ";eff;p_{T}(GeV/c);cent" , 200, 1, 3, 25, 0.5,3.0,16,0,16 );
 
 
   h_multEv  = fs->make<TH1D>("multEv" , ";cent" , 16 , 0, 16 );
+  h_pT  = fs->make<TH1D>("pT" , "" , 1000 , 0, 100 );
+  h_eta  = fs->make<TH1D>("eta" , "" , 96 , -2.4, 2.4 );
+  h_Mult  = fs->make<TH1D>("Mult" , "" , 300 , 0, 3000 );
+  
+  h_M0  = fs->make<TH1D>("Mult_0" , "" , 400 , 0, 4000 );
+  h_M1  = fs->make<TH1D>("Mult_1" , "" , 375 , 0, 3750 );
+  h_M2  = fs->make<TH1D>("Mult_2" , "" , 350 , 0, 3500 );
+  h_M3  = fs->make<TH1D>("Mult_3" , "" , 325 , 0, 3250 );
+  h_M4  = fs->make<TH1D>("Mult_4" , "" , 300 , 0, 3000 );
+  h_M5  = fs->make<TH1D>("Mult_5" , "" , 275 , 0, 2750 );
+  h_M6  = fs->make<TH1D>("Mult_6" , "" , 250 , 0, 2500 );
+  h_M7  = fs->make<TH1D>("Mult_7" , "" , 225 , 0, 2250 );
+  h_M8  = fs->make<TH1D>("Mult_8" , "" , 200 , 0, 2000 );
+  h_M9  = fs->make<TH1D>("Mult_9" , "" , 175 , 0, 1750 );
+  h_M10  = fs->make<TH1D>("Mult_10" , "" , 150 , 0, 1500 );
+  h_M11  = fs->make<TH1D>("Mult_11" , "" , 125 , 0, 1250 );
+  h_M12  = fs->make<TH1D>("Mult_12" , "" , 100 , 0, 1000 );
+  h_M13  = fs->make<TH1D>("Mult_13" , "" , 75 , 0, 750 );
+  h_M14  = fs->make<TH1D>("Mult_14" , "" , 50 , 0, 500 );
+  h_M15  = fs->make<TH1D>("Mult_15" , "" , 25 , 0, 250 );
 
   h_W2  = fs->make<TH1D>("W2" , ";cent" , 16 , 0, 16 );
   h_W22  = fs->make<TH1D>("W22" , ";cent" , 16 , 0, 16 );
@@ -1634,14 +1669,14 @@ Qnana::initHistos(const edm::Service<TFileService> & fs)
   h_CumEightt  = fs->make<TH2D>("CumEightt" , ";vn;cent" , 5, 0, 5, 16 , 0, 16 );  h_Eight_vt  = fs->make<TH2D>("Eight_vt" , ";vn;cent" , 5, 0, 5, 16 , 0, 16 );  h_ErrEight_vt  = fs->make<TH2D>("ErrEight_vt" , ";vn;cent" , 5, 0, 5, 16 , 0, 16 );
   h_CumTent  = fs->make<TH2D>("CumTent" , ";vn;cent" , 5, 0, 5, 16 , 0, 16 );  h_Ten_vt  = fs->make<TH2D>("Ten_vt" , ";vn;cent" , 5, 0, 5, 16 , 0, 16 );  h_ErrTen_vt  = fs->make<TH2D>("ErrTen_vt" , ";vn;cent" , 5, 0, 5, 16 , 0, 16 );
 ////
-*/
+
 
 }
 
 bool
 Qnana::passesTrackCuts(const reco::Track & track, const reco::VertexCollection & vertex, double chi2n)
 {
-/*   if ( ! applyTrackCuts_ ) return true;
+   if ( ! applyTrackCuts_ ) return true;
 
    math::XYZPoint vtxPoint(0.0,0.0,0.0);
    double vzErr =0.0, vxErr=0.0, vyErr=0.0;
@@ -1673,14 +1708,16 @@ Qnana::passesTrackCuts(const reco::Track & track, const reco::VertexCollection &
 
    if(nhits < nhitsMin_ ) return false;
 
+/*
    int count = 0;
    for(unsigned i = 0; i < algoParameters_.size(); i++){
       if( algo == algoParameters_[i] ) count++;
    }
+*/
    if(chi2n > chi2nMax_ ) return false;
 
  //  if ( algo == 6 && trkMva < 0.98 ) return false;
-*/
+
    return true;
 }
 
